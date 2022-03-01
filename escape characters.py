f2e9c2758864f535bf528e00e2faca799c7de5e9
gharikrish <99989717+gharikrish@users.noreply.github.com>
2022-03-01 23:46:07 +0530

    Add files via upload

---
+++ escape characters.py
@@ -0,0 +1,29 @@
+print("your \"s\" truly")
+
+print("Hello \"python\" students")
+
+print("your\'s truly")  # to insert one single quote
+
+print("He's \"funny\".")
+
+print("hello\"") # to insert one double quote
+
+print("python\\datascience") # to insert one slash
+
+print("Hello\nWorld")
+
+print("Hello\nworld\rfine")
+
+print("Hello\nworld\rpython")
+
+print("Hello\tworld")
+
+print("Hello\bWorld") # will erase one character
+
+print("Hello\b\bWorld")
+
+print("Hello\vWorld")
+
+print("\110\145\154\154\157\40\127\157\162\154\144\41")
+
+print("\x48\x65\x6c\x6c\x6f\x20\x57\x6f\x72\x6c\x64\x21")
